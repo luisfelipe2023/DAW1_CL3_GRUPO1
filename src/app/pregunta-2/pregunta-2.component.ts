80c63a73c6838e7ebb8523f8aa8b4a79e60894c8
PatrickMontes <alexandermontes30p@hotmail.com>
2024-05-10 12:46:17 -0500

    ts listo

--- src/app/pregunta-2/pregunta-2.component.ts
+++ src/app/pregunta-2/pregunta-2.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pregunta-2',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './pregunta-2.component.html',
   styleUrl: './pregunta-2.component.scss'
 })
 export class Pregunta2Component {
+  numero: number =0;
+  unidades: number = 0;
+  decenas: number=0;
+
+  mostrarResultado = false;
+
+  descomponerNumero(): void {
+    if (this.numero >= 100 && this.numero <= 999) {
+      this.unidades = this.numero % 10;
+      this.decenas = Math.floor((this.numero % 100) / 10);
+    } else {
+      alert("Ingrese un número de 3 cifras!!");
+      this.numero = 0;
+      this.unidades = 0;
+      this.decenas = 0;
+    }
+
+    this.mostrarResultado = true;
+  }
 
 }
