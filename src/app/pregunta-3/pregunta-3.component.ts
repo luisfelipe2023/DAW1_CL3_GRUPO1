47ea9070452619d384e654001325f0c68a93750f
AdrianLlontop <rizzaga1234@gmail.com>
2024-05-10 15:19:04 -0500

    pregunta 3 hecha

---
+++ src/app/pregunta-3/pregunta-3.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pregunta-3',
+  templateUrl: './pregunta-3.component.html',
+  styleUrls: ['./pregunta-3.component.scss']
+})
+export class Pregunta3Component {
+  numero: number = 0; // Definición de la propiedad 'numero'
+  mensaje: string = '';
+
+  verificarNumeroPerfecto(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const numero = inputElement.valueAsNumber;
+    this.numero = numero;
+    let sumaDivisores = 0;
+    for (let i = 1; i < this.numero; i++) {
+      if (this.numero % i === 0) {
+        sumaDivisores += i;
+      }
+    }
+    if (sumaDivisores === this.numero) {
+      this.mensaje = 'Es perfecto';
+    } else {
+      this.mensaje = 'No es perfecto';
+    }
+  }
+}
