35c6d38f357dc56604ed75ac842e6e6f9b882a40
PatrickMontes <alexandermontes30p@hotmail.com>
2024-05-10 12:57:19 -0500

    Pregunta 2.2 Solucionado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Pregunta1Component } from './pregunta-1/pregunta-1.component';
+import { Pregunta2Component } from './pregunta-2/pregunta-2.component';
 
 export const routes: Routes = [
-    {path :"pregunta1",component : Pregunta1Component}
+    {path :"pregunta1",component : Pregunta1Component},
+    {path: "pregunta2", component: Pregunta2Component}
 ];
